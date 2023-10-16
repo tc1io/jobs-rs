aab55bc42e20fea996ea35ba69371d03cc79f39c
Ashesh Mishra <asheshmishra@apples-MacBook-Pro.local>
2023-10-16 17:49:37 +0530

    working state

--- examples/job_repo_demo.rs
+++ examples/job_repo_demo.rs
@@ -1,18 +1,9 @@
-// use std::fmt::Error;
-use async_trait::async_trait;
-use chrono::{TimeZone, Utc};
-use futures::{FutureExt, TryFutureExt};
-// use jobs::{JobError, JobInfo, JobManager, JobRepo, JobRunner, LockData, LockRepo, Schedule};
-use jobs::job::{Job, JobName, Schedule};
+use jobs::job::{Job, JobName, JobRepo, Schedule};
 use jobs::{
-    error::Error, job::JobAction, job::JobRepo, lock::LockRepo, manager::JobManager, repos,
+   repos,
 };
 use pickledb::{PickleDb, PickleDbDumpPolicy, SerializationMethod};
-use serde::{Deserialize, Serialize};
-use std::ops::Add;
-use std::sync::Arc;
-use tokio::sync::RwLock;
-use tokio::time::{interval, sleep, Duration};
+
 
 #[tokio::main]
 async fn main() {
@@ -21,11 +12,13 @@ async fn main() {
         PickleDbDumpPolicy::AutoDump,
         SerializationMethod::Json,
     );
-
     let mut repo = repos::pickledb::Repo::new(db);
-
+    let name  = JobName {
+        name: "dummmy".to_string(),
+    };
     repo.create_job(Job {
-        name: Default::default(),
+        // name: JobName { name: "dummy".to_string() },
+        name: name.clone(),
         state: vec![],
         schedule: Schedule {
             expr: "".to_string(),
@@ -36,54 +29,18 @@ async fn main() {
     .await
     .unwrap();
 
-    // let mut db_client = PickleDb::new(
-    //     "example.db",
-    //     PickleDbDumpPolicy::AutoDump,
-    //     SerializationMethod::Json,
-    // );
-    // let mut lock_client = PickleDb::new(
-    //     "lock.db",
-    //     PickleDbDumpPolicy::AutoDump,
-    //     SerializationMethod::Json,
-    // );
-    // let db_repo = DbRepo {
-    //     db: Arc::new(RwLock::new(db_client)),
-    // };
-    // let lock_repo = DbRepo {
-    //     db: Arc::new(RwLock::new(lock_client)),
-    // };
-    //
-    // let job1 = JobImplementer {
-    //     // name: "".to_string(),
-    // };
-    // let job2 = JobImplementer {
-    //     // name: "".to_string(),
-    // };
-    //
-    // let mut manager = JobManager::<DbRepo, DbRepo>::new(db_repo, lock_repo);
-    // manager.register(String::from("project-updater"), job1).await;
-    // manager.register(String::from("project-puller"), job2).await;
-    // let _ = manager.start().await.unwrap();
+    repo.save_state( name.clone(), vec![])
+        .await
+        .unwrap();
+
+    match repo
+        .get_job(name)
+        .await{
+        Ok(Val) => {
+            println!("{:?}", Val)
+        }
+        Err(_) => {
+            println!("Not found")
+        }
+    }
 }
-
-//
-// #[async_trait]
-// impl LockRepo for DbRepo {}
-
-// #[derive(Clone, Serialize, Deserialize, Debug)]
-// struct Project {
-//     name: String,
-//     id: i32,
-//     lifecycle_state: String,
-//     updated: String,
-// }
-//
-// #[async_trait]
-// impl JobAction for JobImplementer {
-//     async fn call(&mut self, name: String, state: Vec<u8>) -> Result<Vec<u8>, Error> {
-//         dbg!("call");
-//         dbg!(name);
-//         let state = Vec::new();
-//         Ok(state)
-//     }
-// }
--- src/repos/pickledb.rs
+++ src/repos/pickledb.rs
@@ -3,6 +3,7 @@ use crate::job::{Job, JobName, JobRepo};
 use async_trait::async_trait;
 use pickledb::PickleDb;
 use std::sync::Arc;
+use chrono::Utc;
 use tokio::sync::RwLock;
 
 #[derive(Clone)]
@@ -21,6 +22,7 @@ impl Repo {
 #[async_trait]
 impl JobRepo for Repo {
     async fn create_job(&mut self, job: Job) -> Result<bool, Error> {
+        println!("create job");
         self.db
             .write()
             .await
@@ -29,54 +31,28 @@ impl JobRepo for Repo {
             .map_err(|e| Error::GeneralError {
                 description: "job creation failed".to_string(),
             })?
-    }
 
+    }
     async fn get_job(&mut self, name: JobName) -> Result<Option<Job>, Error> {
-        todo!()
+        Ok(self
+            .db
+            .write()
+            .await
+            .get::<Job>((&name).into()))
     }
 
     async fn save_state(&mut self, name: JobName, state: Vec<u8>) -> Result<bool, Error> {
-        todo!()
+        let mut job = self.get_job(name.clone()).await.unwrap().unwrap();
+        job.state = state;
+        // let name1 = name.clone()
+        // job.last_run = Utc::now().timestamp_millis();
+        self.db
+            .write()
+            .await
+            // .map_err(|e| JobError::DatabaseError(e.to_string()))?
+            .set((&name).into(), &job)
+            .unwrap();
+        println!("state saved");
+        Ok(true)
     }
-    // async fn create_job(&mut self, job: Job) -> Result<bool, Error> {
-    //     dbg!("2");
-    //     println!("create_job");
-    //     let name = (&job.name).into();
-    //     self.db
-    //         .write()
-    //         .await
-    //         .set(name, &job)
-    //         .map(|_| Ok(true))
-    //         .map_err(|e| Error::GeneralError { description: "job creation failed".to_string() })?
-    // }
-    //
-    // async fn get_job(&mut self, name: JobName) -> Result<Option<Job>, Error> {
-    //     Ok(self
-    //         .db
-    //         .write()
-    //         .await
-    //         // .map_err(|e| Error::GeneralError { description: "".to_string() })?
-    //         .get::<Job>((&name).into()))
-    // }
-    //
-    // async fn save_state(&mut self, name: JobName, state: Vec<u8>) -> Result<bool, Error> {
-    //     let mut job = self.get_job(name.clone()).await.unwrap().unwrap();
-    //     job.state = state;
-    //     // let name1 = name.clone()
-    //     job.last_run = Utc::now().timestamp_millis();
-    //     dbg!("{:?}", job.last_run);
-    //     self.db
-    //         .write()
-    //         .await
-    //         // .map_err(|e| JobError::DatabaseError(e.to_string()))?
-    //         .set((&name).into(), &job)
-    //         .unwrap();
-    //     println!("state saved");
-    //     Ok(true)
-    // }
 }
-// struct JobImplementer {
-//     // name: String,
-//     // db: PickleDb,
-//     // project: Project,
-// }
