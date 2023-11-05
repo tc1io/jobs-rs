7bf515791359ae10790a902ae67c49f165e301e2
Aravind Ragavendran <aravindragavendran@gmail.com>
2023-11-05 09:42:05 +0530

    fix build issues

--- examples/simple.rs
+++ examples/simple.rs
@@ -51,10 +51,10 @@ async fn main() {
     // );
     let _ = manager.start_all().await.unwrap();
     sleep(Duration::from_secs(4)).await;
-    manager
-        .stop_by_name(String::from("project-updater"))
-        .await
-        .unwrap();
+    // manager
+    //     .stop_by_name(String::from("project-updater"))
+    //     .await
+    //     .unwrap();
     sleep(Duration::from_secs(20)).await;
 }
 
